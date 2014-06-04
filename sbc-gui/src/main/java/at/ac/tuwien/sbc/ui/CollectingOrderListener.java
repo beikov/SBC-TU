c513148e0d91eef757c94174f55f54b8b7543a6b
Manuel <Manuel@Manuel-PC>
2014-06-04 13:41:34 +0200

    fixed some bugs

--- sbc-gui/src/main/java/at/ac/tuwien/sbc/ui/CollectingOrderListener.java
+++ sbc-gui/src/main/java/at/ac/tuwien/sbc/ui/CollectingOrderListener.java
@@ -7,27 +7,29 @@ import java.util.List;
 
 public class CollectingOrderListener implements OrderListener {
 
-    private final Runnable listener;
-    private OrderList orderList;
+	private final Runnable listener;
+	private OrderList orderList;
 
-    public CollectingOrderListener(OrderList orderList, Runnable listener) {
-        this.orderList = orderList;
-        this.listener = listener;
-    }
+	public CollectingOrderListener(OrderList orderList, Runnable listener) {
+		this.orderList = orderList;
+		this.listener = listener;
+	}
 
-    @Override
-    public void onOrderAdded(List<Order> orders) {
-        orderList.addAll(orders);
-        listener.run();
-    }
+	@Override
+	public void onOrderAdded(List<Order> orders) {
+		orderList.addAll(orders);
+		listener.run();
+	}
 
-    @Override
-    public void onOrderClockFinished(List<Clock> clocks) {
-        for (Clock clock : clocks) {
-            orderList.getOrder(clock.getOrderId())
-                .addFinishedClock(clock);
-        }
+	@Override
+	public void onOrderClockFinished(List<Clock> clocks) {
+		for (Clock clock : clocks) {
+			if(clock.getOrderId() != null){
+				orderList.getOrder(clock.getOrderId())
+				.addFinishedClock(clock);
+			}
+		}
 
-    }
+	}
 
 }
